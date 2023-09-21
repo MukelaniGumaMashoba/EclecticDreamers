1003c93386528adc6cc3435f3499e83a80e20ae6
Mukelani Guma Mashoba <mukelanilastborn@gmail.com>
2023-09-21 23:19:00 +0200

    changes

--- planetearth/src/screens/Dashboard.js
+++ planetearth/src/screens/Dashboard.js
@@ -1,16 +1,35 @@
-import React from 'react'
-import Logo from '../components/Logo'
-
-import { Text, View } from 'react-native'
+import React from 'react';
+import Menu from '../components/menu';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function Dashboard({ navigation }) {
   return (
-    <View>
-      <Logo />
-      <Text>Welcome 💫</Text>
-      <Text>
-        Congratulations you are logged in.
+    <View style={styles.container}>
+      <Text style={styles.welcomeText}>Welcome 💫</Text>
+      <Text style={styles.loggedInText}>
+        Congratulations, you are logged in.
       </Text>
+      <Menu /> 
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 20,
+    color: '#333',
+  },
+  loggedInText: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#666',
+  },
+});
