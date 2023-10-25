d2c394002b46f618a070ce9d4baef9e8fc032957
Hlalii <132391272+Hlalii@users.noreply.github.com>
2023-10-25 18:56:48 +0200

    Update Vehicle.js

--- Calculator/Screens/Vehicle.js
+++ Calculator/Screens/Vehicle.js
@@ -138,7 +138,7 @@ const VehicleForm = ({ onSubmit }) => {
     <SafeAreaView>
       <ScrollView contentContainerStyle={styles.scrollContainer}>
         <View style={styles.container}>
-          <TouchableOpacity style={styles.backStyle} onPress={() => navigation.navigate('Main App')}>
+          <TouchableOpacity style={styles.backStyle} onPress={() => navigation.navigate('Calculator')}>
             <Text style={styles.buttonText}>BACK</Text>
           </TouchableOpacity>
           <Image source={iconImage} style={styles.icon} />
